44e0fef1f51fa2266b9a17530eaa011da23a9459
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-05-09 07:55:13 +0530

    Update image_drawing to add rectangle and circles on the image

--- OpenCV-101/3. opencv-drawing/image_drawing.py
+++ OpenCV-101/3. opencv-drawing/image_drawing.py
@@ -1,22 +1,26 @@
+# Usage
+# python image_drawing.py -i ../../Data/Input/Dog.jpg
+
 # Load libraries cv2, argparse
 import cv2
 import argparse
 
 # Add arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str, default="adrian.png", help="Image")
+ap.add_argument("-i", "--image", type=str, help="Input image", required=True)
 args = vars(ap.parse_args())
 
 # Load original image
 image = cv2.imread(args["image"])
+print('Shape of image: ', image.shape)
 cv2.imshow("Original", image)
 
 # Draw rectangle, circle around face, eyes respectively
 green = (0, 255, 0)
-cv2.rectangle(image, (100, 90), (225, 260), green, 2)
+cv2.rectangle(image, (2200, 350), (2700, 900), green, 5)
 
 red = (0, 0, 255)
-cv2.circle(image, (150, 160), 10, red, -1)
-cv2.circle(image, (195, 170), 10, red, -1)
+cv2.circle(image, (2300, 600), 40, red, -1)
+cv2.circle(image, (2600, 600), 40, red, -1)
 cv2.imshow("Update", image)
 cv2.waitKey(0)
