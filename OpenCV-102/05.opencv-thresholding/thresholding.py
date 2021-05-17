df458a04456ae4ad4a04f8f28b9d6cc587b5c0ae
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-05-17 10:04:45 +0530

    Renamed to thresholding.py

--- OpenCV-102/05.opencv-thresholding/thresholding.py
+++ OpenCV-102/05.opencv-thresholding/thresholding.py
@@ -1,5 +1,5 @@
 # Usage
-# python simple_thresholding.py -i ../../Data/Input/coins01.png
+# python thresholding.py -i ../../Data/Input/coins01.png
 
 # Import libraries
 import cv2
@@ -40,9 +40,30 @@ for check in thresh_checks:
     print('Threshold check={}'.format(check))
     cv2.imshow(f"Thresholded {check}", thresh)
     cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('Grayscale Blurred', blurred)
 
 # We can use thresh_inv as a mask and then bitwise_and with original image to segment coins out
 _, thresh_inv = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY_INV)
+result = cv2.bitwise_and(image, image, mask=thresh_inv)
+cv2.imshow("Segmented", result)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('Grayscale Blurred', blurred)
+
+# Otsu's thresholding
+# As in Simple thresholding, we had to manually adjust the Threshold check value in order work with different images.
+# With Otsu, we don't have to provide Threshold check value, it automatically finds the value for thresholding.
+(T, thresh_inv) = cv2.threshold(blurred, 0,
+                                255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+# It gives Threshold check value as 191.0
+cv2.imshow(f"Otsu Thresholded w/ {T}", thresh_inv)
+cv2.waitKey(0)
+
+# Similarly, we can use this for segmentation
 result = cv2.bitwise_and(image, image, mask=thresh_inv)
 cv2.imshow("Segmented", result)
 
