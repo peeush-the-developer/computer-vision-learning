90f2bce4d8bd069f9147bdfe960dcdf35e43c1d0
Peeush Agarwal <agarwal.peeush.developer@gmail.com>
2021-05-30 11:03:46 +0530

    Add logic to guess aruco markers

---
+++ OpenCV-104/05.opencv-aruco-markers/guess_aruco_type.py
@@ -0,0 +1,49 @@
+# Usage
+# python guess_aruco_type -i ../../Data/Input/png/ArUco_markers.png
+
+# Import libraries
+import cv2
+import argparse
+
+# Add command line arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, required=True,
+                help="Input image path containing aruco markers")
+args = vars(ap.parse_args())
+
+# Load the image
+image = cv2.imread(args["input"])
+
+# Define aruco dictionary
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+aruco_params = cv2.aruco.DetectorParameters_create()
+for name, dict_ in ARUCO_DICT.items():
+    aruco_dict = cv2.aruco.Dictionary_get(dict_)
+    results = cv2.aruco.detectMarkers(
+        image, aruco_dict, parameters=aruco_params)
+
+    if len(results[0]) > 0:
+        print(f'{len(results[0])} Aruco markers detected in {name} dict')
